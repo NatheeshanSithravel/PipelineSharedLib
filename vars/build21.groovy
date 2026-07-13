a5306dbaf888f4db1d2713328738ee398dff2d47
Natheeshan Sithravel <natheeshan.20244025@iit.ac.lk>
2026-07-13 14:01:04 +0530

    Modify BASE_URL for rollback file download
    Update BASE_URL to include project name in download script.

--- vars/build21.groovy
+++ vars/build21.groovy
@@ -218,7 +218,7 @@ Please review the CodeScanner Dashboard for details.
             sh """
                 set -e
 
-                BASE_URL=${NEXUS_URL}/repository/${REPO}
+                BASE_URL=${NEXUS_URL}/repository/${REPO}/${pipelineParams.projectName}
 
                 echo "Downloading ${params.ROLLBACK_FILE}"
 
