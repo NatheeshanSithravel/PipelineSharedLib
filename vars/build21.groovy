cf6647bcb576f9a23799490f10fd0911535a35e2
Natheeshan Sithravel <natheeshan.20244025@iit.ac.lk>
2026-07-28 14:59:18 +0530

    Update build21.groovy

--- vars/build21.groovy
+++ vars/build21.groovy
@@ -288,7 +288,8 @@ Please review the CodeScanner Dashboard for details.
         }
     }
 }
-
+			
+}
         post {
             failure {
                 jiraUpdate(params.ISSUE_KEY, 31, env, currentBuild)
