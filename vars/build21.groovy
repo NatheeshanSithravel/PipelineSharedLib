e51a9888c3e317d9cfb1222ff0f92714e80b53d5
NatheeshK <natheeshshaan@gmail.com>
2026-08-03 11:02:24 +0530

    minor changes

--- vars/build21.groovy
+++ vars/build21.groovy
@@ -102,7 +102,7 @@ def call(body) {
                          expression { false }
                          anyOf {
                              branch 'staging'
-                             branch 'staging2'
+                             branch 'main'
                              branch 'production'
                         }
                      }
@@ -212,22 +212,39 @@ Please review the CodeScanner Dashboard for details.
 
             stage('Download Rollback Artifact') {
     when {
-        expression { params.ROLLBACK }
+        allOf {
+            expression { params.ROLLBACK }
+            branch 'main'
+        }
     }
     steps {
         script {
+            def warName = sh(
+                script: '''
+                    mvn help:evaluate \
+                      -Dexpression=project.build.finalName \
+                      -q \
+                      -DforceStdout
+                ''',
+                returnStdout: true
+            ).trim()
+
+            echo "Final WAR Name: ${warName}.war"
+
             sh """
                 set -e
+
+                mkdir -p target
 
                 BASE_URL=${NEXUS_URL}/repository/${REPO}/${pipelineParams.projectName}
 
                 echo "Downloading ${params.ROLLBACK_FILE}"
 
                 curl -f -u ${CREDS} \
-                    -o rollback.war \
+                    -o target/${warName}.war \
                     "\${BASE_URL}/${params.ROLLBACK_FILE}"
 
-                ls -lh rollback.war
+                ls -lh target/${warName}.war
             """
         }
     }
@@ -251,56 +268,22 @@ Please review the CodeScanner Dashboard for details.
                 }
             }
             stage('Deploy') {
-    when {
-        expression {
-            (params.PRODUCTION_BUILD == true && params.PASSWORD == 'm0bitel#123' && pipelineParams.platform != 'weblogic') ||
-            (params.PRODUCTION_BUILD == true && params.PASSWORD == 'WLpr0d*' && pipelineParams.platform == 'weblogic') ||
-            (env.BRANCH_NAME == 'dr' && params.PASSWORD == 'm0bitel#123' && pipelineParams.platform != 'weblogic') ||
-            (env.BRANCH_NAME != 'production' &&
-             env.BRANCH_NAME != 'dr' &&
-             env.BRANCH_NAME != 'production2')
-        }
-    }
-
-    steps {
-        script {
-
-            if (params.ROLLBACK) {
-                echo "Deploying rollback artifact..."
-
-                def warName = sh(
-                    script: '''
-                        mvn help:evaluate \
-                          -Dexpression=project.build.finalName \
-                          -q \
-                          -DforceStdout
-                    ''',
-                    returnStdout: true
-                ).trim()
-
-                echo "Final WAR Name: ${warName}.war"
-
-                sh """
-                    mkdir -p target
-                    cp rollback.war target/${warName}.war
-                """
-
-                deploy(pipelineParams.deployEnv ?: env.BRANCH_NAME, pipelineParams, null)
-
-            } else {
-
-                def pom = null
-
-                if (pipelineParams.build_env != 'grunt') {
-                    pom = readMavenPom file: 'pom.xml'
+                when {
+                    expression { (params.PRODUCTION_BUILD == true && params.PASSWORD == 'm0bitel#123' && pipelineParams.platform != 'weblogic') || (params.PRODUCTION_BUILD == true && params.PASSWORD == 'WLpr0d*' && pipelineParams.platform == 'weblogic') || (env.BRANCH_NAME == 'dr' && params.PASSWORD == 'm0bitel#123' && pipelineParams.platform != 'weblogic') || env.BRANCH_NAME != 'production' && env.BRANCH_NAME != 'dr' && env.BRANCH_NAME != 'production2'}
                 }
-
-                deploy(pipelineParams.deployEnv ?: env.BRANCH_NAME, pipelineParams, pom)
+                steps {
+                    script {
+                       if ( pipelineParams.build_env != 'grunt'){
+                        pom = readMavenPom file: 'pom.xml'
+                       }
+                      def deployEnv = pipelineParams.deployEnv ?: env.BRANCH_NAME
+					  deploy(deployEnv, pipelineParams, pom)
+                    }
+                    // deploy(env.BRANCH_NAME, pipelineParams, pom)
+                  	
+                }
             }
-        }
-    }
-}
-}
+   }
 
 
         post {
